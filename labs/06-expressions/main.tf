75ee6c540b1cac1f4bdbe0c76df9282c2f9ab06d
Santhosh Kumar <santhosh.ravichandran@essent.nl>
2026-03-19 11:40:13 +0100

    :memo: introduce more challenge in the lab 06 expression

--- labs/06-expressions/main.tf
+++ labs/06-expressions/main.tf
@@ -20,6 +20,26 @@ locals {
   }
 }
 
+locals {
+  apps = {
+    "frontend" = {
+      dept        = "marketing"
+      open_ports  = [80, 443]
+      is_public   = true
+    }
+    "database" = {
+      dept        = "engineering"
+      open_ports  = [5432]
+      is_public   = false
+    }
+    "internal-api" = {
+      dept        = "engineering"
+      open_ports  = [8080, 9090]
+      is_public   = false
+    }
+  }
+}
+
 # 2. THE RESOURCE: Using for_each to iterate
 resource "local_file" "service_config" {
   # We iterate over our map. 
@@ -38,7 +58,26 @@ Labels:       ${join(", ", each.value.tags)}
 EOT
 }
 
-# 4. THE OUTPUT: Using a 'for' expression to summarize
-output "deployment_summary" {
-  value = [for name, config in local.services : "Deployed ${name} on port ${config.port}"]
+#Resource Generation: Use a for_each to create a local_file for every app in the map.
+# Naming Convention: The filename should be: firewall-<APP_NAME>-<DEPT>.cfg.
+# The "Filter" Challenge: Only create files for apps where is_public is false. (We want to skip the frontend for this specific internal firewall task).
+# Content Formatting: Inside the file, the open_ports list must be converted into a single string separated by pipes (e.g., 80|443).
+# The Output: Create an output that returns a list of all the departments represented in the apps map (Bonus: try to make the department names uppercase).
+
+resource "local_file" "firewall-configuration" {
+    for_each = { for key, value in local.apps: key => value if !value.is_public} 
+    filename = "firewall-${each.key}-${each.value.dept}.cfg"
+    content = <<EOT
+        open_ports = ${join("|", [for open_port in each.value.open_ports: tostring(open_port)])}
+        department = ${each.value.dept}
+    EOT
+}
+
+output departments {
+  value = distinct([
+    for app in local_file.firewall-configuration: app.filename])
+}
+
+output "department_names_in_uppercase" {
+  value = distinct([for key, value in local.apps: upper(value.dept)])
 }
--- labs/06-expressions/output.tf
+++ labs/06-expressions/output.tf
@@ -1,13 +1,3 @@
-output "first_az" {
-  # Accessing a list by index [0]
-  value = local.az_list[0]
-}
-
-output "prod_instance_type" {
-  # Accessing a map by key
-  value = local.env_config["prod"].instance_type
-}
-
 output "deployment_summary" {
   value = [for name, config in local.services : "Deployed ${name} on port ${config.port}"]
 }
