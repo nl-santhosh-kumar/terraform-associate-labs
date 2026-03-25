a5e10726cfae1af714ae94d8a8a3d6a2c98246d3
Santhosh Kumar <nl.santhosh.kumar@gmail.com>
2026-03-25 10:11:58 +0100

    :memo: custom domain setup with Route 53

--- labs/aws/static-site-with-cloud-front/route53.tf
+++ labs/aws/static-site-with-cloud-front/route53.tf
@@ -1,10 +1,13 @@
 # Create a Route 53 record to point the custom domain to the CloudFront distribution
 resource "aws_route53_record" "cloudfront" {
-  for_each = aws_cloudfront_distribution.s3_distribution.aliases # Create a Route 53 record for each alias defined in the CloudFront distribution
-  zone_id = data.aws_route53_zone.my_domain_zone.zone_id # Reference the Route 53 hosted zone for the custom domain
-  type    = "CNAME" # Use CNAME record to point to the CloudFront distribution
-  ttl     = 300 # Set a low TTL for quick propagation
-  name = each.value # Use the alias name defined in the CloudFront distribution as the record name
+  # Iterates through the aliases you defined in your CloudFront resource
+  for_each = aws_cloudfront_distribution.s3_distribution.aliases 
+  
+  zone_id = data.aws_route53_zone.my_domain_zone.zone_id
+  name    = each.value
+  
+  # Use "A" for an Alias record pointing to a CloudFront distribution
+  type    = "A" 
 
   alias {
     name                   = aws_cloudfront_distribution.s3_distribution.domain_name
